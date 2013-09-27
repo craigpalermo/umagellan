922ae82fb88b0fa258770fabd4e6ba79b2bc36c3
unscsprt <cpalermo2@gmail.com>
2013-09-26 21:24:40 -0400

    asd

--- umagellan/wsgi.py
+++ umagellan/wsgi.py
@@ -24,7 +24,7 @@ if path not in sys.path:
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
-os.environ["DJANGO_SETTINGS_MODULE_UMAGELLAN"] = "umagellan.settings"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "umagellan.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
