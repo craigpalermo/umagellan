490e54caf6c9b2e97d5aef3fabbf0b1cd13c7b46
unscsprt <cpalermo2@gmail.com>
2013-09-26 21:42:31 -0400

    Added virtualenv support to wsgi.py

--- umagellan/wsgi.py
+++ umagellan/wsgi.py
@@ -15,16 +15,29 @@ framework.
 """
 import os
 import sys
+import site
 
-path = '/var/webfiles_django/umagellan'
-if path not in sys.path:
-    sys.path.append(path)
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('~/.virtualenvs/umagellan/lib/python2.6/site-packages')
 
-# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
-# if running multiple sites in the same mod_wsgi process. To fix this, use
-# mod_wsgi daemon mode with each site in its own daemon process, or use
-# os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "umagellan.settings")
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/var/webfiles_django/umagellan')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'
+
+# Activate your virtual env
+activate_env=os.path.expanduser("~/.virtualenvs/umagellan/bin/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
+
+# path = '/var/webfiles_django/umagellan'
+# if path not in sys.path:
+#     sys.path.append(path)
+# 
+# # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
+# # if running multiple sites in the same mod_wsgi process. To fix this, use
+# # mod_wsgi daemon mode with each site in its own daemon process, or use
+# # os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "umagellan.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
